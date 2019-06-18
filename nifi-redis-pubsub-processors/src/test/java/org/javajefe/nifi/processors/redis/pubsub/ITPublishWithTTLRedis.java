56f22bc074cd5124f2e01280834c9ea4c0b28407
Wojciech Bartosiak <wojtek@flipsports.com>
2019-06-18 22:20:22 +0200

    redis execution with variables as attrbiturtes

---
+++ nifi-redis-pubsub-processors/src/test/java/org/javajefe/nifi/processors/redis/pubsub/ITPublishWithTTLRedis.java
@@ -0,0 +1,94 @@
+package org.javajefe.nifi.processors.redis.pubsub;
+
+import org.apache.nifi.components.AllowableValue;
+import org.apache.nifi.reporting.InitializationException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import redis.clients.jedis.JedisPubSub;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
+/**
+ * This is an integration test that is meant to be run against a real Redis instance.
+ */
+public class ITPublishWithTTLRedis extends ITAbstractRedis {
+
+    @Before
+    public void setup() throws IOException, InitializationException {
+        setup(new PublishWithTTLRedis());
+    }
+
+    @After
+    public void teardown() throws IOException {
+        super.teardown();
+    }
+
+    @Test
+    public void testLPUSH() {
+        generateMessages(10);
+        init(PublishWithTTLRedis.LPUSH_MODE);
+        publish();
+
+        assertEquals("list", jedis.type(KEY));
+        List<String> value = jedis.lrange(KEY, 0, -1);
+        Collections.reverse(value);
+        assertArrayEquals(messages.toArray(), value.toArray());
+    }
+
+    @Test
+    public void testRPUSH() {
+        generateMessages(10);
+        init(PublishWithTTLRedis.RPUSH_MODE);
+        publish();
+
+        assertEquals("list", jedis.type(KEY));
+        List<String> value = jedis.lrange(KEY, 0, -1);
+        assertArrayEquals(messages.toArray(), value.toArray());
+    }
+
+    @Test
+    public void testPUBLISH() throws InterruptedException {
+        generateMessages(10);
+        init(PublishWithTTLRedis.PUBLISH_MODE);
+        final Deque<String> channelsDeque = new ConcurrentLinkedDeque<>();
+        final Deque<String> messagesDeque = new ConcurrentLinkedDeque<>();
+        ExecutorService threadPool = Executors.newSingleThreadExecutor();
+        threadPool.submit(() -> jedis.subscribe(new JedisPubSub() {
+            @Override
+            public void onMessage(String channel, String message) {
+                channelsDeque.addLast(channel);
+                messagesDeque.addLast(message);
+            }
+        }, KEY));
+
+        publish();
+
+        threadPool.shutdown();
+        threadPool.awaitTermination(2, TimeUnit.SECONDS);
+        assertArrayEquals(messages.toArray(), messagesDeque.toArray());
+        assertTrue("Invalid channel", channelsDeque.stream().allMatch(channel -> channel.equals(KEY)));
+    }
+
+    private void init(AllowableValue queueMode) {
+        testRunner.setProperty(PublishWithTTLRedis.REDIS_CONNECTION_POOL, "redis-connection-pool");
+        testRunner.setProperty(PublishWithTTLRedis.QUEUE_MODE, queueMode);
+        testRunner.setProperty(PublishWithTTLRedis.CHANNEL_OR_LIST, KEY);
+    }
+
+    private void publish() {
+        // queue a flow file to trigger the processor and executeProcessor it
+        messages.forEach(payload -> testRunner.enqueue(payload));
+        testRunner.run(messages.size());
+        testRunner.assertAllFlowFilesTransferred(PublishWithTTLRedis.REL_SUCCESS, messages.size());
+    }
+}
